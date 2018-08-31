7ab47074d8988e57d3e1d8ed10f5c9b7ed87ae9b
robert <robertnmoffat@gmail.com>
2018-08-30 20:44:46 -0700

    missed vert and face count error output

--- tinyrenderer/model.cpp
+++ tinyrenderer/model.cpp
@@ -33,6 +33,7 @@ Model::Model(const char *filename) : verts_(), faces_() {
 			faces_.push_back(f);
 		}
 	}
+	std::cerr << "# v#" << verts_.size() << " f# " << faces_.size() << std::endl;
 }
 
 Model::~Model() {}
