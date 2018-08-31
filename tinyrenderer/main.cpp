2ffe9db7e5c946231a1e60f0847fe48145abfa2e
robert <robertnmoffat@gmail.com>
2018-08-30 22:49:49 -0700

    fully creates wireframe from obj file

--- tinyrenderer/main.cpp
+++ tinyrenderer/main.cpp
@@ -1,7 +1,11 @@
 #include "tgaimage.h"
+#include "model.h"
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
+Model *model = NULL;
+const int width = 800;
+const int height = 800;
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
 
@@ -16,20 +20,66 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
 	}
 }
 
-void drawRandomLines(int amount, TGAImage image) {
-	for (int i = 0; i < amount; i++) {
-		line(rand() % 100, rand() % 100, rand() % 100, rand() % 100, image, white);
+void line(Vec2i v0, Vec2i v1, TGAImage &image, TGAColor color) {
+
+	int xDiff = v1.x - v0.x, yDiff = v1.y - v0.y;
+	int length = sqrt(xDiff*xDiff + yDiff*yDiff);
+	float its = 1. / (float)length;
+
+	for (float t = 0.; t < 1; t += its) {
+		int x = round(v0.x*(1. - t) + v1.x*t);
+		int y = round(v0.y*(1. - t) + v1.y*t);
+		image.set(x, y, color);
 	}
 }
 
-int main(int argc, char** argv) {
-	TGAImage image(100, 100, TGAImage::RGB);
-	//image.set(52, 41, red);
+void drawRandomLines(int amount, TGAImage &image) {
+	for (int i = 0; i < amount; i++) {
+		line(rand() % width, rand() % height, rand() % width, rand() % height, image, white);
+	}
+}
 
-	drawRandomLines(10, image);	
+void drawLineModel(TGAImage &image) {
+	for (int i = 0; i<model->nfaces(); i++) {
+		std::vector<int> face = model->face(i);
+		for (int j = 0; j<3; j++) {
+			Vec3f v0 = model->vert(face[j]);
+			Vec3f v1 = model->vert(face[(j + 1) % 3]);
+			int x0 = (v0.x + 1.)*width / 2.;
+			int y0 = (v0.y + 1.)*height / 2.;
+			int x1 = (v1.x + 1.)*width / 2.;
+			int y1 = (v1.y + 1.)*height / 2.;
+			line(x0, y0, x1, y1, image, white);
+		}
+	}
+}
+
+void drawTriangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color) {
+	line(t0, t1, image, color);
+	line(t1, t2, image, color);
+	line(t2, t0, image, color);
+}
+
+int main(int argc, char** argv) {
+	if (argc == 2) {
+		model = new Model(argv[1]);
+	}
+	else {
+		model = new Model("obj/head.obj");
+	}
+
+	TGAImage image(width, height, TGAImage::RGB);
+	//image.set(52, 41, red);
+	
+	//drawRandomLines(10, image);	
+
+	drawLineModel(image);
+
+	//drawTriangle(Vec2i(10, 20), Vec2i(30, 30), Vec2i(50, 50), image, white);
+
 
 	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
 	image.write_tga_file("output.tga");
-	
+	delete model;
 	return 0;
 }
--- tinyrenderer/model.cpp
+++ tinyrenderer/model.cpp
@@ -8,32 +8,30 @@
 Model::Model(const char *filename) : verts_(), faces_() {
 	std::ifstream in;
 	in.open(filename, std::ifstream::in);
-	
 	if (in.fail()) return;
-
 	std::string line;
 	while (!in.eof()) {
 		std::getline(in, line);
-		std::istringstream iss(line.c_str());//c_str() returns a pointer to a c style string. null terminated
+		std::istringstream iss(line.c_str());//c_str() returns pointer to c style string
 		char trash;
 		if (!line.compare(0, 2, "v ")) {
 			iss >> trash;
 			Vec3f v;
-			for (int i = 0; i < 3; i++) iss >> v.raw[i];
+			for (int i = 0; i<3; i++) iss >> v.raw[i];
 			verts_.push_back(v);
 		}
-		else if (line.compare(0, 2, "f ")) {
+		else if (!line.compare(0, 2, "f ")) {
 			std::vector<int> f;
 			int itrash, idx;
 			iss >> trash;
 			while (iss >> idx >> trash >> itrash >> trash >> itrash) {
-				idx--; //in wavefront obj all indices start at 1, not 0
+				idx--; // in wavefront obj all indices start at 1, not zero
 				f.push_back(idx);
 			}
 			faces_.push_back(f);
 		}
 	}
-	std::cerr << "# v#" << verts_.size() << " f# " << faces_.size() << std::endl;
+	std::cerr << "# v# " << verts_.size() << " f# " << faces_.size() << std::endl;
 }
 
 Model::~Model() {}
@@ -46,10 +44,10 @@ int Model::nfaces() {
 	return (int)faces_.size();
 }
 
-Vec3f Model::vert(int i) {
-	return verts_[i];
-}
-
 std::vector<int> Model::face(int idx) {
 	return faces_[idx];
+}
+
+Vec3f Model::vert(int i) {
+	return verts_[i];
 }
